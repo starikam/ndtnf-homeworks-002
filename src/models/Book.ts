28ebb4637d4f6c50df5eac799e65d1f968d6fe39
m.viktorov <m.viktorov@starikam.org>
2025-01-12 18:26:02 +0300

    alls done

---
+++ src/models/Book.ts
@@ -0,0 +1,37 @@
+import { model, Schema } from 'mongoose'
+import { IBook } from '../types'
+
+const bookSchema = new Schema<IBook>({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    default: ''
+  },
+  authors: {
+    type: String,
+    default: ''
+  },
+  favorite: {
+    type: String,
+    default: ''
+  },
+  fileCover: {
+    type: String,
+    default: ''
+  },
+  fileName: {
+    type: String,
+    default: ''
+  },
+  fileBook: {
+    type: String,
+    default: ''
+  }
+})
+
+console.log(bookSchema)
+
+export const BookModel = model('Book', bookSchema)
